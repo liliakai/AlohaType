2aaf8a4ffa91aa75f6b2a7a07848fc70db04c5ae
Lilia Kai <liliakai@gmail.com>
2025-12-11 15:11:40 -1000

    Fix 404s

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 'base' is required for GitHub Pages to serve assets correctly from a subdirectory
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false
